c4601cbf3ff690329e6d2b6770348a1b4de28dee
IftaherSayem <iftahersayem205@gmail.com>
2024-10-16 09:03:04 +0600

    Create 1828B.cpp

---
+++ 900 Ratings/1828B.cpp
@@ -0,0 +1,55 @@
+
+/*
+========================================
++          IFTAHER HOSSEN SAYEM        +
++            IIUC,Dept. of CSE         +
+========================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int m=1e9 + 7;
+void test()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+int finding(char ch)
+{
+    return ch-'A'+1;
+}
+int posi(int h)
+{
+    return h+'a'-1;
+}
+bool deco(pair <int,int>&a,pair<int,int>&b)
+{
+    return a.second>b.second;
+}
+
+bool inco(pair <ll,ll>&a,pair<ll,ll>&b)
+{
+    return a.second<b.second;
+}
+int main()
+{
+    //test();
+    ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+    ll n;
+    cin >> n;
+    while(n--)
+    {
+        ll a,b,c;
+        cin >> a;
+        vector <int> ar(a+1);
+        int gc=0;
+        for (int i=1; i<=a; i++)
+        {
+            cin >> ar[i];
+            int diff=abs(ar[i]-i);///finding position difference from main array to sorted array
+            gc=__gcd(gc,diff);
+        }
+        cout << gc << endl;
+    }
+}
