5d18e0c70bf0a61c386695bbf850c1a7c6d41431
IftaherSayem <iftahersayem205@gmail.com>
2024-10-17 17:39:50 +0600

    Create 1559A.cpp

---
+++ 900 Ratings/1559A.cpp
@@ -0,0 +1,52 @@
+
+/*
+========================================
++          IFTAHER HOSSEN SAYEM        +
++            IIUC,Dept. of CSE         +
+========================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int m=1e9 + 7;
+void test()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+bool chk(int a)
+{
+    if(a==2 || a==3 || a==5) return true;
+    for(int i=2; i*i<=a; i++)
+    {
+        if(a%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    //test();
+    ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+    ll n;
+    cin >> n;
+    while(n--)
+    {
+        ll a;
+        cin >> a;
+        vector <int> ar(a);
+        for (int i=0; i<a; i++)
+        {
+            cin >> ar[i];
+        }
+        int ans=ar[0];
+        for (int i=1;i<a;i++)
+        {
+           ans&=ar[i];
+        }
+        cout << ans << endl;
+    }
+}
