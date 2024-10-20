4d505da06eef78c9b8d0d9a45b707dac8cc0accb
IftaherSayem <iftahersayem205@gmail.com>
2024-10-20 16:39:39 +0600

    Create 688B.cpp

---
+++ 1000 Ratings/688B.cpp
@@ -0,0 +1,46 @@
+
+/*
+========================================
++          IFTAHER HOSSEN SAYEM        +
++            IIUC,Dept. of CSE         +
+========================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int m=1e9 + 7;
+void test()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+bool chk(int a)
+{
+    if(a==2 || a==3 || a==5) return true;
+    for(int i=2; i*i<=a; i++)
+    {
+        if(a%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    //test();
+    ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+    //ll n;
+    //cin >> n;
+    //while(n--)
+    //{
+    ll a,b;
+    //cin >> a;
+    string s;
+    cin >> s;
+    string sr=s;
+    reverse(sr.begin(),sr.end());
+    cout << s << sr << endl;
+    //}
+}
