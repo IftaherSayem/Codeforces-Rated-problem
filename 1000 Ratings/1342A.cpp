b07c22fab0b915b8db0577bc3f3f5f1543953fe0
IftaherSayem <iftahersayem205@gmail.com>
2024-10-23 23:24:14 +0600

    Create 1342A.cpp

---
+++ 1000 Ratings/1342A.cpp
@@ -0,0 +1,49 @@
+/*
+========================================
++          IFTAHER HOSSEN SAYEM        +
++            IIUC,Dept. of CSE         +
+========================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int m=1e9 + 7;
+void test()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+bool chk(int a)
+{
+    if(a==2 || a==3 || a==5) return true;
+    for(int i=2; i*i<=a; i++)
+    {
+        if(a%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    //test();
+    ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+    ll n;
+    cin >> n;
+    while(n--)
+    {
+        ll a,b,c,d;
+        cin >> a >> b;
+        cin >> c >> d;
+        ll ab=abs(a-b);
+        ll mul=ab*c;
+        a=min(a,b);
+        b=min(a,b);
+        ll first=(a+b)*c;
+        ll secnd=a*d;
+        mul+=(ll)min(first,secnd);
+        cout << mul << endl;
+    }
+}
