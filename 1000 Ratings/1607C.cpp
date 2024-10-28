caadf2205806799d020e2f1e90b542ebb3840bcc
IftaherSayem <iftahersayem205@gmail.com>
2024-10-28 11:21:01 +0600

    Create 1607C.cpp

---
+++ 1000 Ratings/1607C.cpp
@@ -0,0 +1,69 @@
+/*
+========================================
++          IFTAHER HOSSEN SAYEM        +
++            IIUC,Dept. of CSE         +
+========================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define el endl;
+const int m=1e9 + 7;
+void test()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+bool chk(int a)
+{
+    if(a==2 || a==3 || a==5) return true;
+    for(int i=2; i*i<=a; i++)
+    {
+        if(a%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+ll power(ll x, ll y)
+{
+    ll val = 1;
+
+    while (y > 0)
+    {
+        if (y % 2 != 0)
+            val = val * x;
+        y = y / 2;
+        x = x * x;
+    }
+    return val;
+}
+
+int main()
+{
+    //test();
+    ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+    ll n;
+    cin >> n;
+    while(n--)
+    {
+        ll a,b,c,d;
+        cin >> a;
+        vector <int>ar(a);
+        for (int i=0; i<a; i++)
+        {
+          cin >> ar[i];
+        }
+        sort(ar.begin(),ar.end());
+        int maxi=ar[0];
+        for (int i=1;i<a;i++)
+        {
+           int diff=ar[i]-ar[i-1];
+           maxi=max(maxi,diff);
+        }
+        cout << maxi << el;
+    }
+}
