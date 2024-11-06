577700ba2a3dc19a9e8a9793e0b6b5882057b632
IftaherSayem <iftahersayem205@gmail.com>
2024-11-06 08:40:26 +0600

    Create 1204A.cpp

---
+++ 1000 Ratings/1204A.cpp
@@ -0,0 +1,66 @@
+/*
+========================================
++          IFTAHER HOSSEN SAYEM        +
++            IIUC,Dept. of CSE         +
+========================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define el endl
+const int m=1e9 + 7;
+void test()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+bool chk(int a)
+{
+    if(a==2 || a==3 || a==5) return true;
+    for(int i=2; i*i<=a; i++)
+    {
+        if(a%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+ll power(ll x, ll y)
+{
+    ll val = 1;
+
+    while (y > 0)
+    {
+        if (y % 2 != 0)
+            val = val * x;
+        y = y / 2;
+        x = x * x;
+    }
+    return val;
+}
+
+int main()
+{
+    //test();
+    ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+    //ll n;
+    //cin >> n;
+    //while (n--)
+    //{
+    //ll a,b,c,d;
+    string s;
+    cin >> s;
+    string st="1";
+    int to=0;
+    while(st<s && st.length()<=s.length())
+    {
+        to++;
+        st+="00";///in every time there are increasing 2 zeroes so add it
+    }
+    cout << to << el;
+
+    //}
+}
