3e7476425849be363ba955f4d01fd7cff948c974
IftaherSayem <iftahersayem205@gmail.com>
2024-11-06 21:47:51 +0600

    Create 1611C.cpp

---
+++ 1000 Ratings/1611C.cpp
@@ -0,0 +1,71 @@
+/*
+========================================
++          IFTAHER HOSSEN SAYEM        +
++            IIUC,Dept. of CSE         +
+========================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define el endl
+const int m=1e9 + 7;
+void test()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+bool chk(int a)
+{
+    if(a==2 || a==3 || a==5) return true;
+    for(int i=2; i*i<=a; i++)
+    {
+        if(a%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+ll power(ll x, ll y)
+{
+    ll val = 1;
+
+    while (y > 0)
+    {
+        if (y % 2 != 0)
+            val = val * x;
+        y = y / 2;
+        x = x * x;
+    }
+    return val;
+}
+
+int main()
+{
+    //test();
+    ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+    ll n;
+    cin >> n;
+    while (n--)
+    {
+        ll a,b,c,d;
+        cin >> a;
+        vector <int>ar(a);
+        for (int i=0;i<a;i++)
+        {
+           cin >> ar[i];
+        }
+        if(ar[0]!=a && ar[a-1]!=a)
+        {
+           cout << -1 << el;
+           continue;
+        }
+        for (int i=a-1;i>=0;i--)
+        {
+           cout << ar[i] << " ";
+        }
+        cout << el;
+    }
+}
