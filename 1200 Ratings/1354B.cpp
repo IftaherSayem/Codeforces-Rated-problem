bd68880fce66824093abf91d415e024b75b9c478
IftaherSayem <iftahersayem205@gmail.com>
2025-11-05 22:15:06 +0600

    Create 1354B.cpp

---
+++ 1200 Ratings/1354B.cpp
@@ -0,0 +1,203 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_IN_OUT ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+#define loop(a,k) for (int i=a;i<k;i++)
+#define onescount(x) __builtin_popcount(x) ///count 1s in a unsigned integer
+#define lcm(a,b) (a*b)/__gcd(a,b)
+#define yes cout << "YES" << el;
+#define no cout << "NO" << el;
+#define gcd(a,b) __gcd(a,b)
+#define ll long long
+#define ull unsigned long long
+#define el "\n"
+#define sp " "
+#define MOD 998244353
+const int m=1e9 + 7;
+void test()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+bool chk(ll a,ll b)
+{
+    for(int i=a; i*i<=a; i++)
+    {
+        if(a%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+vector<bool> chkprime(1e6+1,true);
+vector<int> primes;
+void sieve()
+{
+    chkprime[0]=chkprime[1]=false;
+    for (int i=2;i*i<=1e6;i++)
+    {
+        if(chkprime[i])
+        {
+            for (int j=i*i;j<=1e6;j+=i)
+            {
+                chkprime[j]=false;
+            }
+        }
+    }
+    for (int i=2;i<=1e6;i++)
+    {
+        if (chkprime[i]) primes.push_back(i);
+    }
+}
+ll power(ll x, ll y)
+{
+    ll val = 1;
+
+    while (y > 0)
+    {
+        if (y % 2 != 0)
+            val = val * x;
+        y = y / 2;
+        x = x * x;
+    }
+    return val;
+}
+
+void binary_modular_exponentiation(ll  w,ll x,ll y)
+{
+    ll ans=1;
+    while(x)
+    {
+        if(x%2==1)
+        {
+            ans=(ans*w);
+            x--;
+        }
+        else
+        {
+            w=(w*w);
+            x/=2;
+        }
+    }
+    cout << y%ans << el;
+}
+
+int Euclid(int x, int y)
+{
+    while(y!=0)
+    {
+        int rem=x%y;
+        x=y;
+        y=rem;
+    }
+    return x;
+}
+bool prfcsq(ll a)
+{
+    ll squ = sqrt(a);
+    return (squ * squ == a);
+}
+
+int nxpr(int a)
+{
+    for (int i=a;;i++)
+    {
+        bool chk=true;
+        for (int j=2;j*j<=i;j++)
+        {
+            if(i%j==0)
+            {
+                chk=false;
+                break;
+            }
+        }
+        if(chk) return i;
+    }
+}
+bool xsx(vector<int>&ar,int oth)
+{
+    int l=0,r=ar.size()-1;
+    int ans=1;
+    while(l<r)
+    {
+        if(ar[l]==oth)
+        {
+           l++;
+           continue;
+        }
+        if(ar[r]==oth)
+        {
+            r--;
+            continue;
+        }
+        if(ar[l]!=ar[r])
+        {
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
+}
+bool palindrome(int a)
+{
+    if (a<0 || (a%10==0 && a!=0)) return false;
+    int rev=0;
+    while (a>rev)
+    {
+        rev=rev*10+ a%10;
+        a/=10;
+    }
+    return a==rev || a==rev/10;
+}
+bool good(ll a,ll b,ll c,ll n)
+{
+    return(n/a)*(n/b)>=c;
+}
+bool cycle(int x,vector<vector<int>> &adj,vector<int>&vis,int par)
+{
+    vis[x]=1;
+    // cout << x << " ";
+    for (int i=0;i<adj[x].size();i++)
+    {
+        int v=adj[x][i];
+        if(!vis[v])
+        {
+            if(cycle(v,adj,vis,x)) return true;
+        }
+        else if(v!=par) return true;
+    }
+    return false;
+}
+int main()
+{
+    FAST_IN_OUT
+    int n,cas=1;
+    cin >> n;
+    while(n--)
+    {
+        // int a,b,c;
+        // cin >> a >> b >> c;
+        string s;
+        cin >> s;
+        int x=s.size();
+        unordered_map<char,int>mp;
+        int l=0,r=0,un=0;
+        int mini=200001;
+        while(r<x)
+        {
+            mp[s[r]]++;
+            if(mp[s[r]]==1) un++;
+            r++;
+            while(un>=3)
+            {
+                mini=min(mini,r-l);
+                mp[s[l]]--;
+                if(mp[s[l]]==0)  un--;
+                l++;
+            }
+        }
+        if(mini==200001) cout << 0 << el;
+        else cout << mini << el;
+   }
+}
